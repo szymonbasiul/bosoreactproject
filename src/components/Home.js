cabcb697c842285dcedece3383c3458b12897ec6
szymo <szymon.basiul@wp.pl>
2021-01-06 00:30:05 +0100

    Added array logic to homepage projects

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,37 +5,60 @@ import project2pic from '../img/project2pic.png';
 import project3pic from '../img/project3pic.png';
 
 function Home(){
-    const loremIpsum =(<div class="pro-text" >Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Pariatur fugit aperiam temporibus ut vel et, 
+    const loremIpsum =(<div className="pro-text" >Lorem ipsum dolor sit amet consectetur adipisicing elit.<br/> Pariatur fugit aperiam temporibus ut vel et,
     id nostrum doloremque animi vero unde,<br/> cumque ipsum optio hic minima impedit quisquam reprehenderit iusto?<br/>
     n commodi animi ducimus neque repellat <br/>dignissimos doloribus ipsam nam labore asperiores,<br/>
 </div>)
- const temporaryDivObject =(
-    <div id="project-container"> 
-            <div class="project-class" id="project1">
-                <img class="pro-image"  src={project1pic}/>
-                <div className="pro-text" >
-                    <ul>First javascript project.<br/>
-                   Tools used:
-                    <li>css</li>
-                    <li>html</li>
-                    <li>js</li>
-                    </ul>
-                    <br/>
-                    Calculator program made for training purposes. <br/>
-                    Project by CodeTeam.
-                </div>
-            </div>
-            <div class="project-class" id="project2">
-                <img class="pro-image"  src={project2pic}/>
-                {loremIpsum}
-            </div>
-            <div class="project-class" id="project3">
-                <img class="pro-image"  src={project3pic} />
-                {loremIpsum}
-            </div>
-    </div>
- )
+    const project1UlItemList = ['css', 'html', 'js'];
+    const project1UlListDisplay = () => {
 
-    return temporaryDivObject
+              return       (<div className="pro-text" >
+                  <ul> First javascript project. <br/>
+                    Tools used: {
+                        project1UlItemList.map(x=><li>{x}</li>)
+                    }</ul>
+                    <br/>Calculator program made for training purposes. <br/>Project by CodeTeam.
+              </div>)
+    }
+
+    const projectClassList = [
+        {
+            src: project1pic
+        },
+        {
+            src: project2pic
+        },
+        {
+            src: project3pic
+        }
+    ]
+    const projectsInContainerDisplay = ()=>{
+        return (<div id="project-container">
+        {projectClassList.map(x=>(
+            <div className="project-class">
+                <img className="pro-image" src={x.src} alt="display project"/>
+                {x.src==project1pic?project1UlListDisplay():loremIpsum}
+            </div>
+        ))}
+        </div>);
+    }
+
+//     const temporaryDivObject =(
+//     <div id="project-container">
+//             <div className="project-class" >
+//                 <img className="pro-image" alt="display project" src={project1pic}/>
+//                    {project1UlListDisplay()}
+//             </div>
+//             <div className="project-class" >
+//                 <img className="pro-image" alt="display project" src={project2pic}/>
+//                 {loremIpsum}
+//             </div>
+//             <div className="project-class" >
+//                 <img className="pro-image" alt="display project" src={project3pic} />
+//                 {loremIpsum}
+//             </div>
+//     </div>
+//  )
+    return projectsInContainerDisplay()
 }
 export default Home;
