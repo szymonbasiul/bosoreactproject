c3f545efbd056154c0a4a6f31a0e3d613df41d19
vilider <andrewbagio@yahoo.com>
2021-01-08 21:50:08 +0000

    contact update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,20 +2,38 @@ import React from 'react';
 import './Contact.css';
 
 const Contact = function(){
+const testTrigger = (event) => {
+    console.log(event.target.value)
 
-    const contactForm = (
-    <div>
+    if(event.target.value){
+        console.log(true);
+    }else{console.log(false)}
+}
+const contactForm = (
+<div>
         <div id="contact-greeter">
             <p>
                 Welcome U! Write to us CzłowieniU!
             </p>
         </div>
-        <div id ="contact-container">
-            <input type="text" name="name" placeholder="John Doe" />
-            <input type="text" name="email" placeholder="John@Doe.Hoe" />
-            <input type="text" name="message" placeholder="Write to us..." />
+    
+    <div id="contact-container">
+        <div id="inputField-container">
+            <input className="input-box" type="text" name="name" placeholder="John Doe"
+            onChange={testTrigger} />
+            <input className="input-box" readOnly={true} type="text" name="email" placeholder="John@Doe.Hoe" />
+            <textarea maxLength="150"  rows="3" readOnly={true}  id="comment-field" type="text" name="message" placeholder="Write to us..." />
         </div>
-    </div>
+        <div id="responsive-container">
+            <div id="responisive-textField">
+                lalalala bamba
+            </div>
+            <div id="button-field">
+
+            </div>
+        </div>
+    </div>    
+</div>
 )
 return contactForm;
 }
