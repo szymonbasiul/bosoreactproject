7663756fd1afbb1cf9315591701b56cb113085a9
vilider <andrewbagio@yahoo.com>
2021-01-20 07:07:32 +0000

    contact page update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,73 +1,60 @@
 import React from 'react';
 import './Contact.css';
-import {useState} from 'react';
+import { useState } from 'react';
 
-const Contact = function(){
-    let inputFieldCheck = {
+const Contact = function () {
 
-        emailField: false,
-        textField: false,
+    const [unblockEmailField, setUnblockEmailField] = useState(true);
+    const [unblockTextField, setUnblockTextField] = useState(true);
 
+    const [beigeText, setBeigeText] =
+        useState("Bezowy tekst");
+
+    const testTrigger = (event) => {
+        console.log(unblockEmailField)
+
+        if (event.target.value) {
+            if (event.target.name == "email") {
+                console.log('text field availible')
+                setUnblockTextField(false)
+
+            }
+            console.log('email availible');
+            setUnblockEmailField(false);
+
+        } else { console.log('something else') }
     }
-    
-    const [unblockField, setUnblockField] = useState(inputFieldCheck);
 
-    const [beigeText, setBeigeText] = 
-    useState("Bezowy tekst");
+    const contactForm = (
 
-    
-
-const testTrigger = (event) => {
-    console.log(unblockField)
-
-    if(event.target.value){
-        if(event.target.name == "email"){
-            console.log(unblockField)
-            inputFieldCheck.textField = true;
-            setUnblockField(inputFieldCheck);
-        }
-        console.log(true);
-        inputFieldCheck.emailField = true;
-        setUnblockField(inputFieldCheck);
-    
-    }else{console.log(false)}
-}
-// let labamba = "Dupajaś";
-// const labambaChange = () => {
-
-//     labamba = "Bardzo ladnie";
-//     console.log("Funkcja uruchomila sie ;)")
-// }
-const contactForm = (
-    
-<div>
-        <div id="contact-greeter">
-            <p>
-                Welcome U! Write to us CzłowieniU!
+        <div>
+            <div id="contact-greeter">
+                <p>
+                    Welcome U! Write to us CzłowieniU!
             </p>
-        </div>
-    
-    <div id="contact-container">
-        <div id="inputField-container">
-            <input className="input-box" type="text" name="name" placeholder="John Doe"
-            onChange={testTrigger} />
-            <input className="input-box" readOnly={!unblockField.emailField} type="text" name="email" placeholder="John@Doe.Hoe" 
-            onChange={testTrigger} />
-            <textarea maxLength="150"  rows="3" readOnly={!unblockField.textField}  id="comment-field" type="text" name="message" placeholder="Write to us..." />
-        </div>
-        <div id="responsive-container">
-            <div id="responisive-textField">
-                {beigeText}
             </div>
-            <div id="button-field" onClick={()=>{setBeigeText("Bezowy owy")}}>
-                Button Fajny
+
+            <div id="contact-container">
+                <div id="inputField-container">
+                    <input className="input-box" type="text" name="name" placeholder="John Doe"
+                        onChange={testTrigger} />
+                    <input className="input-box" readOnly={unblockEmailField} type="text" name="email" placeholder="John@Doe.Hoe"
+                        onChange={testTrigger} />
+                    <textarea maxLength="150" rows="3" readOnly={unblockTextField} id="comment-field" type="text" name="message" placeholder="Write to us..." />
+                </div>
+                <div id="responsive-container">
+                    <div id="responisive-textField">
+                        {beigeText}
+                    </div>
+                    <div id="button-field" onClick={() => { setBeigeText("Bezowy owy") }}>
+                        Button Fajny
 
             </div>
+                </div>
+            </div>
         </div>
-    </div>    
-</div>
-)
-return contactForm;
+    )
+    return contactForm;
 }
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,53 +2,53 @@ import './Navbar.css';
 import homebutton from '../img/homebutton.png';
 import aboutbutton from '../img/aboutbutton.png';
 import contactbutton from '../img/contactbutton.png';
-import React, { useState} from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 
-function Navbutton(props){
-    
+function Navbutton(props) {
+
     const navbutton = (
         <div className="nav-button">
-        <Link to={props.link}>
-            <img className="button-img"
-             src={props.src}
-             title={props.title}
-             alt="cant find"
-             onClick={props.trigger}
-             >
-            </img>
-        </Link>
+            <Link to={props.link}>
+                <img className="button-img"
+                    src={props.src}
+                    title={props.title}
+                    alt="cant find"
+                    onClick={props.trigger}
+                >
+                </img>
+            </Link>
         </div>
     )
 
     return navbutton;
 }
-function Navbar(){
-    const[Page, setPage] = useState((window.location.href).substr(21));
-            
+function Navbar() {
+    const [Page, setPage] = useState((window.location.href).substr(21));
+
     const navbuttons = [
         {
             src: contactbutton,
             link: "/contact",
             title: "contact"
-    },
+        },
         {
             src: aboutbutton,
             link: "/about",
             title: "about"
 
-    },
+        },
         {
             src: homebutton,
             link: "/",
             title: "home"
-    }];
-    const changeState = () =>{
+        }];
+    const changeState = () => {
         setTimeout(() => {
-            setPage((window.location.href).substr(21));  
+            setPage((window.location.href).substr(21));
 
-        },0)
+        }, 0)
     }
 
     const filterDisplay = navbuttons.filter(buttonObject =>
@@ -56,16 +56,16 @@ function Navbar(){
 
     )
     const navbarDisplay = filterDisplay.map(buttonObject =>
-     <Navbutton 
-        key ={buttonObject.title}
-        link = {buttonObject.link}
-        src={buttonObject.src}
-        title={buttonObject.title}
-        trigger={changeState}
+        <Navbutton
+            key={buttonObject.title}
+            link={buttonObject.link}
+            src={buttonObject.src}
+            title={buttonObject.title}
+            trigger={changeState}
         />
     )
 
-    return <div className ="nav-bar">{navbarDisplay}</div>;
+    return <div className="nav-bar">{navbarDisplay}</div>;
 }
 
 export default Navbar;
